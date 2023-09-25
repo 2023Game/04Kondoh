9a0eb035096fdef44c44c33f6e94eef9cc28db00
Kondo-Kazuma-Fukuyama <gc23003@anabuki-net.ne.jp>
2023-09-25 22:16:48 +0900

    CTriangleクラス

--- 3DLv1_vs2019_00/GameProgramming/src/CApplication.cpp
+++ 3DLv1_vs2019_00/GameProgramming/src/CApplication.cpp
@@ -3,6 +3,7 @@
 //OpenGL
 #include "glut.h"
 #include "CVector.h"
+#include "CTriangle.h"
 
 //クラスのstatic変数
 CTexture CApplication::mTexture;
@@ -83,4 +84,21 @@ void CApplication::Update()
 	//描画終了
 	glEnd();
 
+	//三角形クラスのインスタンス作成
+	CTriangle t0;
+	//法線と頂点の設定
+	t0.Vertex(CVector(1.0f, 0.0f, 0.5f), CVector(2.0f, 0.0f, 0.0f), CVector(1.0f, 0.0f, -0.5f));
+	t0.Normal(CVector(0.0f, 1.0f, 0.0f));
+	//三角形の描写
+	t0.Render();
+
+	CTriangle t1;
+	t1.Vertex(CVector(0.0f, 0.5f, 1.0f), CVector(0.0f, 0.0f, 2.0f), CVector(0.0f, -0.5f, 1.0f));
+	t1.Normal(CVector(1.0f, 0.0f, 0.0f));
+	t1.Render();
+
+	CTriangle t2;
+	t2.Vertex(CVector(-0.5f, 1.0f, 0.0f), CVector(0.5f, 1.0f, 0.0f), CVector(0.0f, 2.0f, 0.0f));
+	t2.Normal(CVector(0.0f, 0.0f, 1.0f));
+	t2.Render();
 }

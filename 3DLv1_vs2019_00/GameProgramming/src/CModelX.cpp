c6c11d907f723d24643806dfcb492e247d7c7d46
Kondo-Kazuma-Fukuyama <gc23003@anabuki-net.ne.jp>
2024-04-23 10:28:28 +0900

    02 改

--- 3DLv1_vs2019_00/GameProgramming/src/CModelX.cpp
+++ 3DLv1_vs2019_00/GameProgramming/src/CModelX.cpp
@@ -3,6 +3,29 @@
 #include "CModelX.h"
 #include "glut.h"
 #include "cmodelx.h"
+#include <ctype.h>  //isspace関数の宣言
+/*
+* IsDelimiter(c)
+* cが\t \r \n スペースなどの空白文字
+* または,;"などの文字であれば
+* 区切り文字としてtrueを返す
+*/
+
+bool CModelX::IsDelimiter(char c)
+{
+	//isspace(c)
+	//cが空白文字なら0以外を返す
+	if (isspace(c) != 0)
+		return true;
+	/*
+	strchr(文字列,文字)
+	文字列に文字が含まれていれば、
+	見つかった文字へのポインタを返す
+	見つからなかったらNULLを返す
+	*/
+	if (strchr(",;\"", c) != NULL)
+		return false;
+}
 
 CModelX::CModelX()
 	:mpPointer(nullptr)
@@ -36,17 +59,28 @@ void CModelX::Load(char* file) {
 	fread(buf, size, 1, fp);
 	//最後に¥0を設定する(文字列の終端)
 	buf[size] = '\0';
-	printf("%s\n", mpPointer);
 
 	fclose(fp);    //ファイルをクローズする
 
+	//文字列の最後まで繰り返し
+	while (*mpPointer != '\0') {
+		GetToken();  //単語の取得
+		//単語がFrameの場合
+		if (strcmp(mToken, "Frame") == 0) {
+			printf("%s ", mToken);  //Frame出力
+			GetToken();  //Frame名を取得
+			printf("%s\n", mToken);  //Frame名を出力
+		}
+		//単語がAnimationSetの場合
+		if (strcmp(mToken, "AnimationSet") == 0) {
+			printf("%s ", mToken);  //AnimationSet出力
+			GetToken();  //AnimationSet名を取得
+			printf("%s\n", mToken);  //AnimationSet名を出力
+		}
+	}
+
 	SAFE_DELETE_ARRAY(buf);   //確保した領域を開放する
 
-}
-
-bool CModelX::IsDelimiter(char c)
-{
-	return false;
 }
 
 /*
@@ -76,15 +110,15 @@ char* CModelX::GetToken() {
 	//もしmTokenが//の場合は、コメントなので改行まで読み飛ばす
 	/*
 	strcmp(文字列1,文字列2)
-	
-	
+	文字列１と文字列２が等しい場合、０を返します。
+	文字列１と文字列２が等しくない場合、０以外を返します。
 	*/
 	if (!strcmp("//", mToken)) {
-		//
+		//改行まで読み飛ばす
 		while (*p != '\0' && !strchr("\r\n", *p)) p++;
-		//
+		//読み込み位置の更新
 		mpPointer = p;
-		//
+		//単語を取得する(再帰呼び出し)
 		return GetToken();
 	}
 	return mToken;

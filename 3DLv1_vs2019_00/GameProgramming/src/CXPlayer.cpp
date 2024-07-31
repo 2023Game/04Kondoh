a094350de32cc0b5518e39b628d4a72480c01937
Kondo-Kazuma-Fukuyama <gc23003@anabuki-net.ne.jp>
2024-07-31 10:21:21 +0900

    21

--- 3DLv1_vs2019_00/GameProgramming/src/CXPlayer.cpp
+++ 3DLv1_vs2019_00/GameProgramming/src/CXPlayer.cpp
@@ -4,21 +4,40 @@
 
 void CXPlayer::Update() {
 
-	if (mInput.Key('A')) {
-
-		mRotation.Y(mRotation.Y() + 2.0);
-	}
-	if (mInput.Key('D')) {
-
-		mRotation.Y(mRotation.Y() - 2.0);
-	}
-	if (mInput.Key('W')) {
-		ChangeAnimation(1, true, 60);
-		Position(Position() + mMatrixRotate.VectorZ() * 0.1);
-	}
-	else
+	switch (AnimationIndex())
 	{
-		ChangeAnimation(0, true, 60);
+	case 3:
+		if (IsAnimationFinished())
+		{
+			ChangeAnimation(4, false, 30);
+		}
+		break;
+	case 4:
+		if (IsAnimationFinished())
+		{
+			ChangeAnimation(0, true, 60);
+		}
+	default:
+
+		if (mInput.Key('A')) {
+
+			mRotation.Y(mRotation.Y() + 2.0);
+		}
+		if (mInput.Key('D')) {
+
+			mRotation.Y(mRotation.Y() - 2.0);
+		}
+		if (mInput.Key('W')) {
+			ChangeAnimation(1, true, 60);
+			Position(Position() + mMatrixRotate.VectorZ() * 0.1);
+		}
+		else
+		{
+			ChangeAnimation(0, true, 60);
+		}
+		if (mInput.Key(' ')) {
+			ChangeAnimation(3, false, 30);
+		}
 	}
 
 	CXCharacter::Update();

df3be21dc770e4ccf58fcae762f863d6eb0b03d7
Kondo-Kazuma-Fukuyama <145415705+Kondo-Kazuma-Fukuyama@users.noreply.github.com>
2025-11-06 16:51:59 +0900

    ステートマシンだお

--- untitled/Game/BaseProject/src/CStateMachine.cpp
+++ untitled/Game/BaseProject/src/CStateMachine.cpp
@@ -3,10 +3,17 @@
 
 void CStateMachine::Update()
 {
+	// 現在のステートが nullptrじゃ無かったら
+	// ステートを更新する
+	if (mCurrentState) return mCurrentState->OnUpdate();
 }
 
 void CStateMachine::ChangeState(std::shared_ptr<CStateBase> newState)
 {
+	// 現在のステートが nullptrじゃ無かったら
+	if (mCurrentState) return mCurrentState->OnExit();
+
+
 }
 
 std::shared_ptr<CStateBase> CStateMachine::GetCurrentState() const
